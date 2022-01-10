721acfda2d500badbb3c5e7d201110556fadd271
Jyri Leskinen <jleskinen@hotmail.com>
2022-01-10 14:00:29 +0200

    eka testi

---
+++ app/src/main/java/com/example/olutta/fragment1.kt
@@ -0,0 +1,31 @@
+package com.example.olutta
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
+
+class fragment1 : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view=inflater.inflate(R.layout.fragment_fragment1, container, false)
+
+        val button=view.findViewById<Button>(R.id.fragiin2)
+        button.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_fragment1_to_fragment2)
+        }
+
+        return view
+    }
+
+
+}
